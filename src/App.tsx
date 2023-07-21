c6aca8c0b8c73864f28735ffd5dec1508b338f93
jperezcoca <jperezcoca@ceit.es>
2023-07-21 12:45:29 +0200

    estilos de la calculadora añadidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='calculadora'>
+      <div className='display'>
+        <div id="valor-anterior"></div>
+        <div id="valor-actual"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button className='col-2'>C</button>
+      <button>&larr;</button>
+      <button className='operador'>%</button>
+      <button className='numero'>7</button>
+      <button className='numero'>8</button>
+      <button className='numero'>9</button>
+      <button className='operador'>X</button>
+      <button className='numero'>4</button>
+      <button className='numero'>5</button>
+      <button className='numero'>6</button>
+      <button className='operador'>-</button>
+      <button className='numero'>1</button>
+      <button className='numero'>2</button>
+      <button className='numero'>3</button>
+      <button className='operador'>+</button>
+      <button className='col-2 numero' >0</button>
+      <button className='numero'>.</button>
+      <button className='operador'>=</button>
+    </div>
   )
 }
 
